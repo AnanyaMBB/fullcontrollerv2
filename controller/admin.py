130e602ca8a2b6b7cd8ea14f5d8055e435717ea2
AnanyaMBB <100018705+AnanyaMBB@users.noreply.github.com>
2024-05-10 05:58:03 +0900

    commiting admin.py

--- controller/admin.py
+++ controller/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import FanButtonModel, DuctButtonModel, Mode, ModeElements, DuctPosition
+from .models import FanButtonModel, DuctButtonModel, Mode, ModeElements, DuctPosition, SensorData
 
 admin.site.register(FanButtonModel)
 admin.site.register(DuctButtonModel)
 admin.site.register(Mode)
 admin.site.register(ModeElements)
 admin.site.register(DuctPosition)
+admin.site.register(SensorData)
